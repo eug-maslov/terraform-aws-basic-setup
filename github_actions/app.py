a69074ac8f7fbc50eb12ff3c2cf5819954c99033
evvmaslov <evv.maslov@gmail.com>
2025-06-05 14:34:53 +0300

    Add github actions workflow + OIDC and ecr setup

---
+++ github_actions/app.py
@@ -0,0 +1,2 @@
+
+print("Hello from inside GitHub Actions ECR container!")
---
+++ github_actions/oidc-setup.tf
@@ -0,0 +1,85 @@
+provider "aws" {
+  region  = "eu-north-1"
+  profile = "terraform"
+}
+
+resource "aws_ecr_repository" "github_repo" {
+  name = "terraform-aws-basic-setup"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:eug-maslov/terraform-aws-basic-setup:ref:refs/heads/main",
+        "repo:eug-maslov/terraform-aws-basic-setup:ref:refs/tags/v*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload"
+    ]
+    resources = [aws_ecr_repository.github_repo.arn]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "oidc_policy" {
+  name        = "oidc-policy"
+  description = "An OIDC policy for GitHub Actions to push/pull ECR images"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.oidc_policy.arn
+}
