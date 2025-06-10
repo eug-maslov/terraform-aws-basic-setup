2c2913f8acfe7af014aff13611c1d3293727535d
evvmaslov <evv.maslov@gmail.com>
2025-06-10 11:22:37 +0300

    Updated cloudinit config

--- terraform-basic/ec2.tf
+++ terraform-basic/ec2.tf
@@ -31,25 +31,6 @@ resource "aws_instance" "web_server" {
   
   iam_instance_profile = aws_iam_instance_profile.codedeploy_profile.name
 
-  /* It's better to install codedeploy agent via ansible, but here I wanted to test user data in tf */
-
-  user_data = <<-EOF
-    sudo yum update -y
-    sudo yum install -y ruby wget -y 
-    wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/install -O /tmp/install
-
-    chmod +x /tmp/install
-
-    sudo /tmp/install auto
-
-    sudo systemctl start codedeploy-agent
-    sudo systemctl enable codedeploy-agent
-
-    sudo amazon-linux-extras install nginx1 -y
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-  EOF
-
   tags = {
     Name        = "${var.project_name}-web-server"
     Environment = var.environment
@@ -57,6 +38,35 @@ resource "aws_instance" "web_server" {
     CodeDeploy  = "true"
   }
 }
+
+data "cloudinit_config" "user_data_config" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "install.sh"
+    content_type = "text/x-shellscript"
+    content = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y ruby wget -y
+wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/install -O /tmp/install
+
+chmod +x /tmp/install
+
+# Run the CodeDeploy agent installer. The 'auto' flag handles installation,
+sudo /tmp/install auto
+
+sudo systemctl start codedeploy-agent
+sudo systemctl enable codedeploy-agent
+
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+    EOF
+  }
+}
+
 
 output "web_server_instance_id" {
   value       = aws_instance.web_server.id
